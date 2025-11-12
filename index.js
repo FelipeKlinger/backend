20f27de87a667299fbe60844c52132f29d0ce30b
FelipeKlinger <juanfelipetaborda9@gmail.com>
2025-11-12 15:12:22 +0100

    Antes de produccion

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
 
+app.use(cors()); // middleware to enable CORS
 
 app.use(express.json()); // middleware to parse JSON bodies
 
@@ -75,7 +77,8 @@ app.post('/api/notes', (request, response) => {
 })
 
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
