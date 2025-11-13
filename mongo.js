9fcf28ca70de11369ce54e086bcd8307aada6579
FelipeKlinger <juanfelipetaborda9@gmail.com>
2025-11-13 15:33:22 +0100

    Incluyendo MonogoDB como base de datos

---
+++ mongo.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://felipe:${password}@clusterfelipeklingerdaw.q1htk.mongodb.net/?appName=ClusterFelipeKlingerDAW`;
+
+mongoose.set("strictQuery", false); // strictQuery es una opción de Mongoose que controla cómo se manejan las consultas que contienen campos no definidos en el esquema. Al establecerlo en false, Mongoose permitirá consultas con campos no definidos sin lanzar un error.
+
+mongoose.connect(url); // conecta a la base de datos
+
+const noteSchema = new mongoose.Schema({
+  // define el esquema de la colección
+  content: String,
+  important: Boolean,
+});
+
+const Note = mongoose.model("Note", noteSchema); // crea el modelo Note basado en el esquema noteSchema
+
+const note = new Note({
+  content: "Causa mucho gusto aprender Mongoose",
+  important: true,
+});
+
+note.save().then( result => {
+  console.log('note saved!')
+  console.log(result)
+})
+  
+Note.find({}).then((result) => {
+  result.forEach((note) => {
+    console.log(note);
+  });
+  mongoose.connection.close();
+});
